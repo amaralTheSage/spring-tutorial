7a1e7b56d7549a078854a2fe1a098daeba07848e
Aluno <amaraldesouza9@gmail.com>
2024-11-08 11:14:16 -0300

    update: changes generation strategy to IDENTITY in both models

--- src/main/java/com/projetin/products/Product.java
+++ src/main/java/com/projetin/products/Product.java
@@ -12,7 +12,7 @@ import lombok.Setter;
 @Setter
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @NotBlank
--- src/main/java/com/projetin/users/User.java
+++ src/main/java/com/projetin/users/User.java
@@ -11,7 +11,7 @@ import lombok.Setter;
 @Setter
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @NotBlank
